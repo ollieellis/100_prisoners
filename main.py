497ed8fb6b117220fed44a05e6c315ef2e4d9e6b
Ollie Ellis <43984772+ollieellis@users.noreply.github.com>
2022-08-20 12:04:55 +0100

    Update main.py

--- main.py
+++ main.py
@@ -26,6 +26,31 @@ def add_all_to_each_array(twoD_array, max_value):
             new_arrays = np.append(new_arrays, append_to_each_array(twoD_array, i), axis=0)
     return new_arrays
 
+def next_number_solution(prisoner_num, box_array):
+    prevously_visited = set()
+    stop_searching = False
+    box_2_check = prisoner_num
+    num_opened = 0
+    while(not stop_searching):
+        num_opened += 1
+        stop_searching, box_found = check_stop_searching(num_opened, prisoner_num, prevously_visited, box_array, box_2_check)
+        prevously_visited.add(box_2_check)
+        box_2_check = box_array[box_2_check]
+        print(box_2_check)
+    print(box_found)
+
+def check_stop_searching(num_opened, prisoner_num, boxes_visited, box_array, current_position):
+    if num_opened >= np.floor(np.shape(box_array)[0]/2):
+        return True, False
+    if box_array[current_position] == prisoner_num:
+        return True, True
+    if current_position in boxes_visited: #this is just to stop early as we will just cycle
+        return True, False
+    return False, False
+
+
+
+
 def main():
     n = 4
     all_combos = gen_all_box_combinations(n)
@@ -35,6 +60,16 @@ def main():
     print(all_combos)
     print(np.shape(all_combos))
     
+    test_case = np.array([7, 1, 3, 6, 5, 2, 0, 4])
+    
+    for i in range(6):
+        print("i: = ", i, "     ", test_case)
+        next_number_solution(i, test_case)
+    
+    
+    
 
 if __name__ == "__main__":
     main()
+    
+  
